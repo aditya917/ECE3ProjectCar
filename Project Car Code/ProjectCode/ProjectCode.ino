f12b105e3f43db54c2c5e85350336e79223a000e
Dylan Kwong <alamedadylankwong863@gmail.com>
2024-10-28 11:44:05 -0700

    fixed

--- Project Car Code/ProjectCode/ProjectCode.ino
+++ Project Car Code/ProjectCode/ProjectCode.ino
@@ -14,9 +14,9 @@ uint16_t sensorValues[8];
 void setup() {
   // put your setup code here, to run once:
   ECE3_Init();
-  Serial.begin(9600);
+  Serial.begin(19200);
 
-  pinMode(leftnslp, OUTPUT);
+  pinMode(left_nslp, OUTPUT);
   pinMode(left_dir, OUTPUT);
   pinMode(left_pwm, OUTPUT);
 
@@ -39,12 +39,14 @@ void loop() {
 
 void runSensor(){
   ECE3_read_IR(sensorValues);
-  int error1 = [-392, -1499, -2041, -1047, -1211, -750, -496, -509, -237, -252, -9, 224, 268, 379, 578, 473, 1057, 978, 2002, 1930, 459];
-  int error2 = [-379, -1414, -1978, -1231, -1938, -1441, -1169, -1549, -807, -995, -40, 885, 963, 1199, 1625, 1072, 1863, 1265, 1974, 1771, 390];
+  //int error1[] = [-392, -1499, -2041, -1047, -1211, -750, -496, -509, -237, -252, -9, 224, 268, 379, 578, 473, 1057, 978, 2002, 1930, 459];
+  //int error2[] = [-379, -1414, -1978, -1231, -1938, -1441, -1169, -1549, -807, -995, -40, 885, 963, 1199, 1625, 1072, 1863, 1265, 1974, 1771, 390];
 
-  int fusion1 = sensorValue[0]*(-8) + sensorValue[1]*(-4) + sensorValue[2]*(-2) + sensorValue[3]*(-1) + sensorValue[4]*(1) + sensorValue[5]*(2) + sensorValue[6]*(4) + sensorValue[7]*(8);
-  int fusion2 = sensorValue[0]*(-15) + sensorValue[1]*(-14) + sensorValue[2]*(-12) + sensorValue[3]*(-8) + sensorValue[4]*(8) + sensorValue[5]*(12) + sensorValue[6]*(14) + sensorValue[7]*(18);
+  int fusion1 = (sensorValues[0]*(-8) + sensorValues[1]*(-4) + sensorValues[2]*(-2) + sensorValues[3]*(-1) + sensorValues[4]*(1) + sensorValues[5]*(2) + sensorValues[6]*(4) + sensorValues[7]*(8))/4;
+  int fusion2 = (sensorValues[0]*(-15) + sensorValues[1]*(-14) + sensorValues[2]*(-12) + sensorValues[3]*(-8) + sensorValues[4]*(8) + sensorValues[5]*(12) + sensorValues[6]*(14) + sensorValues[7]*(18))/8;
 
   Serial.println(fusion1);
+  delay(1000);
   Serial.println(fusion2);
+  delay(1000);
 }
